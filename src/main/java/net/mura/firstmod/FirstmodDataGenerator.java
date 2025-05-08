e178140b37fd3b3fe890a4ac157c6317e04d2bf1
nicoluvas <nicolaspoficial@gmail.com>
2025-05-08 17:07:46 -0300

    init

---
+++ src/main/java/net/mura/firstmod/FirstmodDataGenerator.java
@@ -0,0 +1,11 @@
+package net.mura.firstmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class FirstmodDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
