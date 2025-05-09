e51f3ad6064a3eebf689d9cc06ae30237490cf81
nicoluvas <nicolaspoficial@gmail.com>
2025-05-08 21:40:18 -0300

    custom itemgroup

--- src/main/java/net/mura/firstmod/item/ModItems.java
+++ src/main/java/net/mura/firstmod/item/ModItems.java
@@ -1,16 +1,30 @@
 package net.mura.firstmod.item;
 
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemGroups;
+import net.minecraft.item.ItemStack;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import net.mura.firstmod.FirstMod;
 
 public class ModItems {
+
+    public static final RegistryKey<ItemGroup> CUSTOM_ITEM_GROUP_KEY = RegistryKey.of(Registries.ITEM_GROUP.getKey(),
+            Identifier.of(FirstMod.MOD_ID, "item_group"));
+    public static final ItemGroup CUSTOM_ITEM_GROUP = FabricItemGroup.builder()
+            .icon(() -> new ItemStack(ModItems.PINK_THING))
+            .displayName(Text.translatable("itemGroup.firstmod"))
+            .build();
+
+
+
     public static final Item PINK_THING = registerItem("pink_thing", new Item(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(FirstMod.MOD_ID,"pink_thing")))));
     public static final Item RAW_PINK_THING = registerItem("raw_pink_thing", new Item(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(FirstMod.MOD_ID,"raw_pink_thing")))));
 
@@ -21,9 +35,11 @@ public class ModItems {
     public static void registerModItems () {
         FirstMod.LOGGER.info("registering mod items");
 
-        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
-            entries.add(PINK_THING);
-            entries.add(RAW_PINK_THING);
+        Registry.register(Registries.ITEM_GROUP, CUSTOM_ITEM_GROUP_KEY, CUSTOM_ITEM_GROUP);
+
+        ItemGroupEvents.modifyEntriesEvent(CUSTOM_ITEM_GROUP_KEY).register(itemGroup -> {
+            itemGroup.add(PINK_THING);
+            itemGroup.add(RAW_PINK_THING);
         });
     }
 }
